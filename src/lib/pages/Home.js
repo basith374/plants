4d5fd0c2fd7ad37617472185a2d20a1dc808f9d1
Basith Kunimal <basith@intellicar.in>
2021-07-16 23:42:24 +0530

    slideshow

--- src/lib/pages/Home.js
+++ src/lib/pages/Home.js
@@ -1,4 +1,5 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import Categories from "../components/Categories";
 import Plants from "../components/Plants";
@@ -32,7 +33,36 @@ const Padding = styled.div`
   padding: 0 3em;
 `;
 
+function SlideShow({ image }) {
+  return (
+    <AnimatePresence>
+      <div style={{ height: "200px", paddingTop: "30px" }}>
+        <motion.img
+          key={image.src}
+          src={image.src}
+          style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "8px" }}
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: -300, opacity: 0 }}
+        />
+      </div>
+    </AnimatePresence>
+  );
+}
+
+const images = [
+  "http://assets.basithkunimal.com/plant1.png",
+  "http://assets.basithkunimal.com/plant2.png",
+  "http://assets.basithkunimal.com/plant3.png",
+];
+
 export default function Home() {
+  const [image, setImage] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setImage(image + 1);
+    }, 1000);
+  }, [image]);
   return (
     <Container animate={{ y: 0 }} initial={{ y: 10 }}>
       <Padding>
@@ -41,6 +71,7 @@ export default function Home() {
       <Fade />
       <Scroll>
         <Categories />
+        <SlideShow image={{ src: images[image % images.length] }} />
         <Plants />
       </Scroll>
     </Container>
